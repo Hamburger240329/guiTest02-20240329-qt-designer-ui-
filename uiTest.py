3c5ffc8e4b962cad75baba4251d48b2b26d7a955
gyojincompany <luckyred@gmail.com>
2024-03-29 14:37:58 +0900

    ui 수정, 버튼 이벤트 처리 추가

--- uiTest.py
+++ uiTest.py
@@ -11,6 +11,17 @@ class MyWindow(QMainWindow, form_class):
         super().__init__()  # 부모 클래스 생성자 호출
         self.setupUi(self)  # 위에서 불러온 test.ui를 연결
         self.setWindowTitle("연습 프로그램")
+        self.button1.clicked.connect(self.button1_click)
+        # 버튼1이 클릭되면 button1_click 메서드 호출
+        self.button2.clicked.connect(self.button2_click)
+        # 버튼2이 클릭되면 button2_click 메서드 호출
+
+    def button1_click(self):
+        self.label1.setText("HelloWorld!!!")
+
+    def button2_click(self):
+        self.label1.setText("안녕하세요!!")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
